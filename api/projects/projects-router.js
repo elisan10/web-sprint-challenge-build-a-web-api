9a0395c2b599cda2ccf580e701c343a32d9fdd62
Eli <elize.san10@gmail.com>
2021-03-12 11:21:33 -0600

    get projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,4 +5,18 @@ const router = express.Router();
 
 const Project = require("./projects-model");
 
+// PROJECTS ENDPOINTS  -------------------->>>>
+
+// [GET] /api/projects
+
+router.get("/", (req, res) => {
+  Project.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
 module.exports = router;

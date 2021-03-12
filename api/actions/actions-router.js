afe37f4f0f5ff6898d28b5050d8012ae494c1527
Eli <elize.san10@gmail.com>
2021-03-12 11:30:11 -0600

    get actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,4 +5,18 @@ const router = express.Router();
 
 const Action = require("./actions-model");
 
+// ACTIONS ENDPOINTS  ------------------->>>>
+
+// [GET] /api/actions
+
+router.get("/", (req, res) => {
+  Action.get()
+    .then((actions) => {
+      res.json(actions);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
 module.exports = router;

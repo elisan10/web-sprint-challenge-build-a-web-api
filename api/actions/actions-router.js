5da68137efd070b3e8e1daa3bb11678c5a30df88
Eli <elize.san10@gmail.com>
2021-03-12 11:30:40 -0600

    get actions by id

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -19,4 +19,24 @@ router.get("/", (req, res) => {
     });
 });
 
+//[GET] /api/actions/:id
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Action.get(id)
+    .then((actions) => {
+      if (!actions) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified id does not exist" });
+      } else {
+        res.json(actions);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
 module.exports = router;
